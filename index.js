ec6fae6f92f53a53632a276d0fa101b19f4a133b
Adel Fetner <adelensoc@gmail.com>
2022-04-21 11:19:49 -0300

    Modificado .js

--- index.js
+++ index.js
@@ -3,50 +3,39 @@
 let scrollPrevio = window.scrollY;
 
 window.onscroll = () => {
-
     let header = document.getElementById("container-header");
     let scrollActual = window.scrollY;
 
     if (scrollPrevio > scrollActual) {
         header.style.top = "0";
     } else {
-        header.style.top = "-20%"
+        header.style.top = "-20%";
     }
     scrollPrevio = scrollActual;
-
-}
+};
 
 // Redirecciones
 
-let imageHeader = document.querySelector("#header-img")
+const imageHeader = document.getElementById("header-img");
 
-let imageFooter = document.querySelector("#footer-img")
+const imageFooter = document.getElementById("footer-img");
 
 
-imageHeader.onclick = () => {
-    open("#", "_self")
-};
+imageHeader.onclick = () => open("#", "_self")
 
-imageFooter.onclick = () => {
-    open("#", "_self")
-};
+imageFooter.onclick = () => open("#", "_self")
+
 
 // redirecciones de íconos footer-media
 
-let facebookRedirect = document.querySelector("#footer-facebook")
+const facebookRedirect = document.getElementById("footer-facebook");
 
-let instagramRedirect = document.querySelector("#footer-instagram")
+const instagramRedirect = document.getElementById("footer-instagram");
 
-let gMapsRedirect = document.querySelector("#footer-gmaps")
+const gMapsRedirect = document.getElementById("footer-gmaps");
 
-facebookRedirect.onclick = () => {
-    open("https://www.facebook.com/LorisMDulceC/")
-};
+facebookRedirect.onclick = () => open("https://www.facebook.com/LorisMDulceC")
 
-instagramRedirect.onclick = () => {
-    open("https://www.instagram.com/lorismdulcec/")
-};
+instagramRedirect.onclick = () => open("https://www.instagram.com/lorismdulcec/")
 
-gMapsRedirect.onclick = () => {
-    open("https://g.page/jardinInfantes?share")
-};
+gMapsRedirect.onclick = () => open("https://g.page/jardinInfantes?share")
